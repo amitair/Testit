af9e38e38cece358e96b20c8e1e8a4bedd274293
amitair <amitair@gmail.com>
2014-05-14 09:39:39 -0400

    7

---
+++ ConsoleApplication2/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApplication3;
+
+namespace Consoletest3
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual("hello world",ConsoleApplication3.Program.CreateMessage());
+        }
+    }
+}
